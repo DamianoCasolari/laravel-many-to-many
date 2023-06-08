16d34a1c9ec15060017bd49a0ee30ce9fc04a070
DamianoCasolari <damianocasolari@gmail.com>
2023-06-08 16:47:07 +0200

    first commit

---
+++ app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'unique:projects', 'max:150'],
+            'logo' => ['nullable', 'max:255'],
+            'link' => ['nullable'],
+            'functionality' => ['nullable'],
+            'languages_used' => ['nullable', 'max:255'],
+            'type_id' => ['exists:categories,id']   //per evitare manipolazione hacker dell'id
+        ];
+    }
+}
---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'logo', 'link', 'slug', 'languages_used', 'functionality', 'type_id'];
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+}
