8ca14b2f76ca500c2f551b6e010b163f7eeb8493
DamianoCasolari <damianocasolari@gmail.com>
2023-06-09 01:16:25 +0200

    delete counter tecnology

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -29,7 +29,7 @@
                     <th>Id</th>
                     <th>Name</th>
                     <th>Slug</th>
-                    <th>Projects with this tags</th>
+                    <th>N° of Projects with this lenguage</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -40,7 +40,7 @@
                         <td scope="row"> <b>{{ $tag->name }}</b> </td>
                         <td scope="row"> {{ $tag->slug }}</td>
                         <td scope="row">
-                            <div class="badge bg-dark">{{ $tag->projects()->count() }}</div>
+                            {{-- <div class="badge bg-dark">{{ $tag->projects()->count() }}</div> --}}
                         </td>
                         <td scope="row">
                             <button tag="button" class="btn btn-danger fs_13" data-bs-toggle="modal"
